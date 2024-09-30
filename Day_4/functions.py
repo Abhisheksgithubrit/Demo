0e794af747462f42af603b86ae99ceaff837a4f3
Abhishek  N Alavandi <142651328+Abhisheksgithubrit@users.noreply.github.com>
2024-09-30 04:21:26 +0000

    added files

---
+++ Day_4/functions.py
@@ -0,0 +1,47 @@
+# # # def greet(name):
+# # #     print(f"Namaste,{name}! Welcome to the python session")
+# # # greet("Abhi")
+
+# # #---------------------------------------------------
+# # def add_numbers(a,b):
+# #     return a + b
+
+# # result = add_numbers(3,7)
+# # print(result)
+
+# # #-------------------------------------------------------
+
+# # def my_function():
+# #     x = 10 #Local variable
+# #     print(x)
+# # my_function()
+
+# #---------------------------------------------------
+
+# x = 23 #Global variable
+# y = 45
+# def my_function():
+#     z = 4
+#     print(z,x,y)
+   
+# my_function() 
+# print(x,y)
+
+# x = 23
+
+# def my_function():
+#     print(x)
+
+# my_function
+# print(x)
+# #--------------------------------------------------------
+
+def calculate_area(length,width):
+    return length * width
+
+area1 = calculate_area(12,5)
+area2 = calculate_area(3,9)
+
+print(area1)
+print(area2)
+print(area1,area2)
